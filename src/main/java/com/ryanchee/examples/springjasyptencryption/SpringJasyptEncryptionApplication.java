44f149a5757dc4187d4ec843e1f11946ac613776
ryanchee <ryan_ks_chee@manulife.com>
2018-01-22 21:59:02 +0800

    Initial Commit

---
+++ src/main/java/com/ryanchee/examples/springjasyptencryption/SpringJasyptEncryptionApplication.java
@@ -0,0 +1,12 @@
+package com.ryanchee.examples.springjasyptencryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJasyptEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJasyptEncryptionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ryanchee/examples/springjasyptencryption/SpringJasyptEncryptionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ryanchee.examples.springjasyptencryption;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJasyptEncryptionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
